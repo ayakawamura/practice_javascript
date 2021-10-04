0d1bf7529619f95f8cbf81a419274b48488e079b
ayakawamura <ayakawamura.b@gmail.com>
2021-10-04 17:08:27 +0900

    [JS2章]finish

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-alert('Hello World');
+// let hello = "Hello World";
+// alert(hello);
+let int1 = 1;
+// alert(int1);
+let int2 = -10;
+// alert(int2);
+// // 小数点を代入する
+// let float1 = 3.14;
+// alert(float1);
+
+alert(4+3);
+alert(int1-int2);
+alert(int1*int2);
+alert(int1/int2);
+// alert("hello "+"world");
+
+// let str1="Hello";
+// let str2="World!!";
+// alert (str1+str2);
+
+
+// if文
+// let orange = 20;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+
+// // while文(条件に当てはまる間何回でも繰り返す)
+// let max = 5;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+// for文　（）
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
