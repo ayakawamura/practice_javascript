ac3d741e60a68a91adeb8a97ea4d1babfb15715e
ayakawamura <ayakawamura.b@gmail.com>
2021-10-05 14:27:15 +0900

    [JS5章]finish

--- JQuery/app.js
+++ JQuery/app.js
@@ -1,3 +1,39 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+// $(function(){
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// // ボックスを青に
+// $(function(){
+//   $(".box1").css({
+//     "background-color": '#0000FF',
+//     'height': '100px'
+//   });
+// })
+
+// // ボックスを上へスライド
+// $(function(){
+//   $('.box1').slideUp();
+// });
+
+// // 非表示を表示にする
+// $(function(){
+//   $(".box1").show();
+//   $(".box1").css("background-color", '#0000FF');
+// });
+
+// // 表示を非表示に
+// $(function(){
+//   $(".box1").hide();
+// });
+
+$(function(){
+  $(".box1").slideDown
+  (function(){
+    $(".box1").css({
+      "background-color":'#0000FF',
+      "width":"200px",
+      "height":"100px"
+    }).slideUp();
+  });
 });
+
