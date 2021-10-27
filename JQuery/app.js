4e7516edc810ba9fae3327f30a0168bfa1a6f49b
ayakawamura <ayakawamura.b@gmail.com>
2021-10-27 21:51:21 +0900

    [7章まで]

--- JQuery/app.js
+++ JQuery/app.js
@@ -26,14 +26,36 @@
 //   $(".box1").hide();
 // });
 
+// // 赤い箱をスライドダウンさせたあとに青い箱にしてスライドアップさせる　
+// $(function(){
+//   $(".box1").slideDown
+//   (function(){
+//     $(".box1").css({
+//       "background-color":'#0000FF',
+//       "width":"200px",
+//       "height":"100px"
+//     }).slideUp();
+//   });
+// });
+
+// // アウスオーバーとかクリックとか
+// $(function(){
+//   $(".box1").on("click",function(){
+//     $(".box1").addClass("box1-ext");
+//   });
+//   $(".box1").mouseout(function(){
+//     $(".box1").removeClass("box1-ext");
+//   });
+// });
+
 $(function(){
-  $(".box1").slideDown
-  (function(){
-    $(".box1").css({
-      "background-color":'#0000FF',
-      "width":"200px",
-      "height":"100px"
-    }).slideUp();
+  $('.box1').on('click', function(){
+    $(this).slideUp();
   });
 });
 
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
+  });
+});
